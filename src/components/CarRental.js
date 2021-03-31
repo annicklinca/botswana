f36ee94a08322364d785da0566614649305fdef0
annicklinca <lincaannick14@gmail.com>
2021-03-31 22:33:50 +0200

    same design on car rental

--- src/components/CarRental.js
+++ src/components/CarRental.js
@@ -5,6 +5,7 @@ import Navbar from "./navbar";
 import Footer from "./Footer.js";
 import ReactTooltip from 'react-tooltip';
 import { Tooltip } from 'reactstrap';
+import Background from '../images/car.jpg';
 function CarRental() {
 
         const [tooltipOpen, setTooltipOpen] = useState(false);
@@ -13,27 +14,35 @@ function CarRental() {
     return(
         <>
 <Navbar/>
-<div style={{  
-  backgroundImage: "url(" + "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" + ")",
-  backgroundPosition: 'center',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
-  height: 400
-}}>
-<div className=" bg-black bg-opacity-25 ">
+<div className="car" style={{height:"400px"}}>
+            <div className=" bg-black bg-opacity-25 ">
 
-<h1 className="py-12 text-2xl font-bold text-center text-white md:text-6xl">Cheap and Affordable Rental Cars in Botswana.</h1>
-<br/><br/><br/><br/><br/><br/><br/>
+<h1 className="py-12 pt-48 text-4xl font-bold text-center text-white md:text-4xl">Rent and Buy your desired car in Botswana</h1>
+<div className="flex flex-wrap ">
+    <div className="p-4 mt-8 ml-10 md:w-1/2 md:ml-0 md:p-0">
+     <form className="form-group">
+     {/* <select  className="px-12 py-3 font-bold text-white bg-blue-500 rounded cursor-pointer float-center md:float-right md:px-28 text-md md:text-xl hover:bg-blue-900">
+     <option>select location</option>
+         <option>Gabarone</option>
+         <option>Kasane</option>
+         <option>FrancisTown</option>
+       </select> */}
+       </form>
+    </div>
+    
 </div>
-</div>
+            </div>
+
+        </div>
 <div class="mx-3 mt-2">
+<h5 className=" p-2 pt-4 font-bold">Get a car on an affordable price</h5>
 		<div class="row">
  <div class="col-md-3 ">
 <div class="card mb-3 shadow-md">
      <img class="card-img-top"
       src="image/Toyota RAVA4.jpg" style={{ height:200,}} alt=""/>
      <div class="card-body">
-      <h4 class="card-title">Toyota RAVA4</h4>
+      <h4 class="card-title font-bold">Toyota RAVA4</h4>
       <p class="card-text">Location</p>
       <div class="flex">
       <div>
@@ -70,8 +79,8 @@ function CarRental() {
       </div>
       
       </div>
-      <p class="card-text text-md my-4"><code class="text-2xl">$</code> 50 /day</p>
-      <button type="button" class="whitespace-no-wrap inline-flex items-center justify-center px-6 py-1 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-900 hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:shadow-outline-yellow active:bg-blue-700 transition ease-in-out duration-150" data-toggle="modal" data-target="#exampleModalLong">
+      <p class="card-text text-md my-4"><code class="text-2xl text-yellow-500">$</code> 50 /day</p>
+      <button type="button" class="whitespace-no-wrap inline-flex items-center justify-center px-6 py-1 border border-transparent text-base leading-6 font-bold rounded-md text-white bg-green-500 hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:shadow-outline-yellow active:bg-blue-700 transition ease-in-out duration-150" data-toggle="modal" data-target="#exampleModalLong">
       View More
 </button>
 
@@ -80,7 +89,7 @@ function CarRental() {
  <div class="modal-dialog" role="document">
   <div class="modal-content modal-fullscreen">
    <div class="modal-header">
-    <h5 class="modal-title " id="exampleModalLongTitle">Toyota RAVA4</h5>
+    <h5 class="modal-title" id="exampleModalLongTitle">Toyota RAVA4</h5>
     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
      <span aria-hidden="true">&times;</span>
     </button>
@@ -161,7 +170,7 @@ function CarRental() {
      <img class="card-img-top"
       src="image/Toyota Land Cruiser Safari.jpg" style={{ height:200,}} alt=""/>
      <div class="card-body">
-      <h4 class="card-title">Toyota Land Cruiser </h4>
+      <h4 class="card-title font-bold">Toyota Land Cruiser </h4>
       <p class="card-text">Location</p>
       <div class="flex">
       <div>
@@ -198,8 +207,8 @@ function CarRental() {
       </div>
       
       </div>
-      <p class="card-text text-md my-4"><code class="text-2xl">$</code> 80 /day</p>
-      <button type="button" class="whitespace-no-wrap inline-flex items-center justify-center px-6 py-1 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-900 hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:shadow-outline-yellow active:bg-blue-700 transition ease-in-out duration-150" data-toggle="modal" data-target="#exampleModalLong">
+      <p class="card-text text-md my-4"><code class="text-2xl text-yellow-500">$</code> 80 /day</p>
+      <button type="button" class="whitespace-no-wrap inline-flex items-center justify-center px-6 py-1 border border-transparent text-base leading-6 font-bold rounded-md text-white bg-green-500 hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:shadow-outline-yellow active:bg-blue-700 transition ease-in-out duration-150" data-toggle="modal" data-target="#exampleModalLong">
       View More
 </button>
 
@@ -288,7 +297,7 @@ function CarRental() {
      <img class="card-img-top"
       src="image/Luxury Toyota V-8.jpg" style={{ height:200,}} alt=""/>
      <div class="card-body">
-      <h4 class="card-title">Luxury Toyota V-8</h4>
+      <h4 class="card-title font-bold">Luxury Toyota V-8</h4>
       <p class="card-text">Location</p>
       <div class="flex">
       <div>
@@ -325,8 +334,8 @@ function CarRental() {
       </div>
       
       </div>
-      <p class="card-text text-md my-4"><code class="text-2xl">$</code> 70 /day</p>
-      <button type="button" class="whitespace-no-wrap inline-flex items-center justify-center px-6 py-1 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-900 hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:shadow-outline-yellow active:bg-blue-700 transition ease-in-out duration-150" data-toggle="modal" data-target="#exampleModalLong">
+      <p class="card-text text-md my-4"><code class="text-2xl text-yellow-500">$</code> 70 /day</p>
+      <button type="button" class="whitespace-no-wrap inline-flex items-center justify-center px-6 py-1 border border-transparent text-base leading-6 font-bold rounded-md text-white bg-green-500 hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:shadow-outline-yellow active:bg-blue-700 transition ease-in-out duration-150" data-toggle="modal" data-target="#exampleModalLong">
       View More
 </button>
 
@@ -415,7 +424,7 @@ function CarRental() {
      <img class="card-img-top"
       src="image/VIP vehicle.jpg" style={{ height:200,}} alt=""/>
      <div class="card-body">
-      <h4 class="card-title">VIP Vehicle</h4>
+      <h4 class="card-title font-bold">VIP Vehicle</h4>
       <p class="card-text">Location</p>
       <div class="flex">
       <div>
@@ -452,8 +461,8 @@ function CarRental() {
       </div>
       
       </div>
-      <p class="card-text text-md my-4"><code class="text-2xl">$</code> 100 /day</p>
-      <button type="button" class="whitespace-no-wrap inline-flex items-center justify-center px-6 py-1 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-900 hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:shadow-outline-yellow active:bg-blue-700 transition ease-in-out duration-150" data-toggle="modal" data-target="#exampleModalLong">
+      <p class="card-text text-md my-4"><code class="text-2xl text-yellow-500">$</code> 100 /day</p>
+      <button type="button" class="whitespace-no-wrap inline-flex items-center justify-center px-6 py-1 border border-transparent text-base leading-6 font-bold rounded-md text-white bg-green-500 hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:shadow-outline-yellow active:bg-blue-700 transition ease-in-out duration-150" data-toggle="modal" data-target="#exampleModalLong">
       View More
 </button>
 
